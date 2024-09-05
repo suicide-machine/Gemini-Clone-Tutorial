baadf86188f34f9e8b43359e837259c01d98e3ed
Cenote <himangshukalita131@gmail.com>
2024-09-05 21:46:18 +0530

    Gemini API Integration

---
+++ src/config/gemini.js
@@ -0,0 +1,41 @@
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ */
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai"
+
+const apiKey = process.env.VITE_GEMINI_API_KEY
+const genAI = new GoogleGenerativeAI(apiKey)
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+})
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+}
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    // safetySettings: Adjust safety settings
+    // See https://ai.google.dev/gemini-api/docs/safety-settings
+    history: [],
+  })
+
+  const result = await chatSession.sendMessage(prompt)
+  const response = result.response.text()
+  console.log(result.response.text())
+}
+
+export default run
