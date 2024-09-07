7e35b33d76b5732682169b6cb271f615bbb92806
Cenote <himangshukalita131@gmail.com>
2024-09-07 13:37:48 +0530

    Gemini integration part-02

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useContext } from "react"
 import {
   FaCode,
   FaCompass,
@@ -9,8 +9,22 @@ import {
 import { FaMessage } from "react-icons/fa6"
 import { MdAddPhotoAlternate } from "react-icons/md"
 import { IoMdSend } from "react-icons/io"
+import { Context } from "../context/Context"
 
 const MainContent = () => {
+  const {
+    input,
+    setInput,
+    recentPrompt,
+    setRecentPrompt,
+    prevPrompt,
+    setPrevPrompt,
+    showResult,
+    loading,
+    resultData,
+    onSent,
+  } = useContext(Context)
+
   return (
     <div className="flex-1 min-h-screen pb-[15vh] relative">
       <div className="flex items-center justify-between text-xl p-5 text-slate-700">
@@ -67,12 +81,17 @@ const MainContent = () => {
               type="text"
               placeholder="Enter a prompt here..."
               className="flex-1 bg-transparent border-none outline-none p-2 text-lg"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
             />
 
             <div className="flex gap-4 items-center">
               <MdAddPhotoAlternate className="text-2xl cursor-pointer" />
               <FaMicrophone className="text-2xl cursor-pointer" />
-              <IoMdSend className="text-2xl cursor-pointer" />
+              <IoMdSend
+                onClick={() => onSent(input)}
+                className="text-2xl cursor-pointer"
+              />
             </div>
           </div>
 
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,16 +1,32 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 import run from "../config/gemini"
 
 export const Context = createContext()
 
 const ContextProvider = (props) => {
+  const [input, setInput] = useState("")
+  const [recentPrompt, setRecentPrompt] = useState("")
+  const [prevPrompt, setPrevPrompt] = useState([])
+  const [showResult, setShowResult] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [resultData, setResultData] = useState("")
+
   const onSent = async (prompt) => {
     await run(prompt)
   }
 
-  onSent("Who is mother of dragons in Game of Thrones?")
-
-  const contextValue = {}
+  const contextValue = {
+    input,
+    setInput,
+    recentPrompt,
+    setRecentPrompt,
+    prevPrompt,
+    setPrevPrompt,
+    showResult,
+    loading,
+    resultData,
+    onSent,
+  }
 
   return (
     <Context.Provider value={contextValue}>{props.children}</Context.Provider>
